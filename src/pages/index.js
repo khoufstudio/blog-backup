c9ceb0e4cbbf962377385916cca5093fdd3bc3cc
rizqy nurhaqy <rizqy.nurhaqy@gmail.com>
2020-06-06 17:00:26 +0700

    update index dan logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,30 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+//   <Layout>
+//     <SEO title="Home" />
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+    
+//     <Link to="/page-2/">Go to page 2</Link>
+//   </Layout>
+<div style={{ textAlign: 'center'}}>
+  <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, marginRight: 'auto', marginLeft: 'auto' }}>
+    <Image />
+    <p style={{ fontFamily: 'Open Sans', fontWeight: 600, fontSize: 30, letterSpacing: 1.5}}>khoufstudio</p>
+  </div>
+  <div style={{ maxWidth: `600px`, marginTop: `3rem`, marginRight: 'auto', marginLeft: 'auto' }}>
+    <p style={{fontFamily: 'Monospace', fontSize: 14, lineHeight: 0.5}}>
+      Thanks for visiting us, unfortunately this site is under construction. 
+    </p>
+    <p style={{fontFamily: 'Monospace', fontSize: 14, lineHeight: 0.5}}>
+      We hope you be patient and visiting us again next time.
+    </p>
+
+    <div>👍👍👍👍👍</div>
+  </div>
+</div>
 )
 
 export default IndexPage
